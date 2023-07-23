c86eb86de1e7155fdc95b0448cac4452e96cdf75
Riku Miyata <amaguri0408@gmail.com>
2023-07-23 18:58:41 +0900

    .

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
+// const { dynamicImportCSS } = dynamicImportCSS("./style.css");
+// const { css } = dynamicImportCSS("./assets/style.css");
 import css from "./assets/style.css";
 
 Vue.config.productionTip = false
