c81d8999d321ae3eb01957a3f3213ad650d9e435
Riku Miyata <amaguri0408@gmail.com>
2023-07-24 02:10:36 +0900

    add api for apps

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1 class="title">
-      漫画検索
+      コミックサーチ
     </h1>
     <div>
 
@@ -37,9 +37,9 @@
     </div>
     <div>
       <h2>対応しているアプリ一覧</h2>
-      <AppSegment v-for="(app, index) in apps" :key="index"
-        :title="app.title" :imageUrl="app.imageUrl" :timestamp="app.timestamp" 
-        :appStoreUrl="app.appStoreUrl" :googlePlayUrl="app.googlePlayUrl">
+      <AppSegment v-for="(app, index) in apps" :app="app" :key="index"
+        :title="app.name" :imageUrl="app.img_url" :timestamp="app.crawled_at" 
+        :appStoreUrl="app.app_store_url" :googlePlayUrl="app.google_play_url">
       </AppSegment>
       <!-- <AppSegment>
       </AppSegment> -->
@@ -51,8 +51,8 @@
 // @は/srcの同じ意味です
 // import something from '@/components/something.vue';
 import AppSegment from '@/components/AppSegment.vue';
-// import { baseUrl } from '@/assets/config.js';
-// import axios from "axios";
+import { baseUrl } from '@/assets/config.js';
+import axios from "axios";
 
 export default {
   name: 'Home',
@@ -63,32 +63,40 @@ export default {
   // Vue.jsで使う変数はここに記述する
     return {
       search_word: "",
-      apps: [
-        {
-          title: "マンガONE",
-          imageUrl: "https://play-lh.googleusercontent.com/DnytEYH5QAmkuQg6q5yFJBKYZWpLbCda6AUkyCDzfR9IAgkfCH1mGVGYRzddxL0CIX45",
-          timestamp: "2022年9月18日",
-          appStoreUrl: "https://apps.apple.com/jp/app/%E3%83%9E%E3%83%B3%E3%82%AC%E3%83%AF%E3%83%B3-%E5%B0%8F%E5%AD%A6%E9%A4%A8%E3%81%AE%E3%82%AA%E3%83%AA%E3%82%B8%E3%83%8A%E3%83%AB%E6%BC%AB%E7%94%BB%E3%82%92%E6%AF%8E%E6%97%A5%E9%85%8D%E4%BF%A1/id774618503?itsct=apps_box_link&itscg=30200",
-          googlePlayUrl: "https://play.google.com/store/apps/details?id=jp.co.comic.mangaone&hl=ja&gl=US",
-        },
-        {
-          title: "サンデーうぇぶり",
-          imageUrl: "https://play-lh.googleusercontent.com/W93gCYbSERAPXq7czOGTG27W00qXokzuXnfXF2f5EB3BwYn84zL-t38VPFXXqkAD5A",
-          timestamp: "2022年9月18日",
-          appStoreUrl: "https://cdn.www.sunday-webry.com/images/app-store-bage.svg?1663226634",
-          googlePlayUrl: "https://cdn.www.sunday-webry.com/images/google-play-badge.png?1663226634",
-        },
-      ]
+      apps: []
     };
   },
-  computed: {
+  computed: async function() {
   },
   methods: {
     async search() {
       // console.log(this.search_word)
     },
+    async getApps() {
+      console.log("(^^)")
+      // headerを指定する
+      const headers = {'Authorization' : ''};
+  
+      try{
+        const res = await axios.get(baseUrl + '/api/app_status_4front',  { headers });
+        // crawled_atがある場合のみ表示する
+        var apps = []
+        for (var i = 0; i < res.data.data.length; i++) {
+          console.log(res.data.data[i].crawled_at)
+          if (res.data.data[i].crawled_at !== "-") {
+            apps.push(res.data.data[i])
+          }
+        }
+        console.log(apps)
+        this.apps = apps
+      }catch(e) {
+        console.log("error")
+        console.log(e)
+      }
+    }
   },
   created: async function() {
+    await this.getApps()
   }
 }
 </script>
