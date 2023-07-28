f7b25226916a50ce3e0f4fb61cd4e7851efcfac6
Riku Miyata <amaguri0408@gmail.com>
2023-07-29 07:17:10 +0900

    add search page

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,96 +1,75 @@
 <template>
   <div>
     <div>
-
-      <form class="">
-        <div class="">
-          <div id="search_input">
-            <input type="text" :placeholder="searchedWord" v-model="search_word" required>
-            <a @click="search">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352c79.5 0 144-64.5 144-144s-64.5-144-144-144S64 128.5 64 208s64.5 144 144 144z"/></svg>
-            </a>
-          </div>
-        </div>
-      </form>
-
-      </div>
-        {{ searchedWord }}の検索結果
-      <div>
+      <SearchBoxSegment @searchClick="search" :default_search_word="search_word"></SearchBoxSegment>
     </div>
     <div>
+      件数：{{ comics_num }}件
+    </div>
+    <div>
+      <div v-if="null_flag">
+        <p>
+          <br>
+          <span style="font-weight: bold">{{ search_word }}</span>に一致するマンガは見つかりませんでした。
+        </p>
+      </div>
       <SearchedComicSegment v-for="(value, index) in comics" :key="index"
-        :id="value.id" :title="value.title" :author="value.author"
-        :imageUrl="value.imageUrl" :apps="value.apps">
+        :id="value.id" :title="value.title" :author="value.author" :raw_author="value.raw_author"
+        :imageUrl="value.img_url" :apps="value.apps">
       </SearchedComicSegment>
     </div>
   </div>
 </template>
 <script>
-// 必要なものはここでインポートする
-// @は/srcの同じ意味です
-// import something from '@/components/something.vue';
+import SearchBoxSegment from '@/components/SearchBoxSegment.vue';
 import SearchedComicSegment from '@/components/SearcedComicSegment.vue'
-// import { baseUrl } from '@/assets/config.js';
-// import axios from "axios";
+import { baseUrl } from '@/assets/config.js';
+import axios from "axios";
 
 export default {
   name: 'Search',
   components: {
+    SearchBoxSegment,
     SearchedComicSegment,
   },
   data() {
   // Vue.jsで使う変数はここに記述する
     return {
+      null_flag: false,
       search_word: "",
       comics: [
-        {
-          id: "id1",
-          title: "はなにあらし",
-          author: "古鉢るか",
-          imageUrl: "",
-          apps: [
-            {
-              imgSrc: "https://play-lh.googleusercontent.com/W93gCYbSERAPXq7czOGTG27W00qXokzuXnfXF2f5EB3BwYn84zL-t38VPFXXqkAD5A",
-              name: "サンデーうぇぶり"
-            },
-            {
-              imgSrc: "https://play-lh.googleusercontent.com/DnytEYH5QAmkuQg6q5yFJBKYZWpLbCda6AUkyCDzfR9IAgkfCH1mGVGYRzddxL0CIX45",
-              name: "マンガONE"
-            },
-          ]
-        },
-        {
-          id: "id1",
-          title: "はなにあらし",
-          author: "古鉢るか",
-          imageUrl: "",
-          apps: [
-            {
-              imgSrc: "https://play-lh.googleusercontent.com/W93gCYbSERAPXq7czOGTG27W00qXokzuXnfXF2f5EB3BwYn84zL-t38VPFXXqkAD5A",
-              name: "サンデーうぇぶり"
-            },
-            {
-              imgSrc: "https://play-lh.googleusercontent.com/DnytEYH5QAmkuQg6q5yFJBKYZWpLbCda6AUkyCDzfR9IAgkfCH1mGVGYRzddxL0CIX45",
-              name: "マンガONE"
-            },
-          ]
-        },
-      ]
+      ],
+      comics_num: 0,
     };
   },
   computed: {
-    searchedWord() {
-      return this.$route.query.word
-    }
   },
   methods: {
-    async search() {
-      if (this.search_word === this.searchedWord) return 
-      this.$router.push({name: "Search", query: {word: this.search_word}})
-      // console.log(this.search_word)
+    async search(search_word) {
+      this.comics = []
+      this.search_word = search_word
+      this.null_flag = false
+      await this.getSearchResult()
     },
+    async getSearchResult() {
+      try {
+        const res = await axios.get(
+          `${baseUrl}/api/comics?keywords=${this.search_word}`
+        );
+        this.comics = res.data.data
+        console.log(this.comics)
+        this.comics_num = this.comics.length
+        if (this.comics.length === 0) {
+          this.null_flag = true
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
   },
   created: async function() {
+    this.search_word = this.$route.query.search_word
+    await this.getSearchResult();
   }
 }
 </script>
